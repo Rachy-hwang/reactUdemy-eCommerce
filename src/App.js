eb9c44c0711a4bc7856d49aa511f542c65c52df1
Rachel <rachel@rachels-mbp.home>
2020-03-16 17:30:40 +1100

    3 ways to add link to another page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,38 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
 import './App.css';
 
-import HomePage from './component/homepage/homepage.component';
+import HomePage from './pages/homepage/homepage.component';
+
+const HatsPage = props => (
+  <div>
+    <h1>HATS PAGE</h1>
+    {/* <Link to='/'>Home</Link>
+    <button onClick={() => props.history.push('/')}>Home</button>
+    <Link to={`${props.match.url}/13`}>TO HATS 13</Link> */}
+  </div>
+)
+
+const TopicDetail = props => {
+  return (
+    <div>
+      <h1>TOPIC LIST PAGE: {props.match.params.hatId} </h1>
+    </div>
+  )
+}
 
 function App() {
-  return (<div><HomePage /></div>)
+  return (
+    <div>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path='/' component={HomePage} />
+          <Route path='/hats' component={HatsPage} />
+          {/* <Route path='/hats/:hatsId' component={TopicDetail} /> */}
+        </Switch>
+      </BrowserRouter>
+    </div>
+  )
 }
 
 export default App;
